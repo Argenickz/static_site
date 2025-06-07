eb856cf1278f06b153f124353ced67ff6b7ac852
Argenickz <argenickz@gmail.com>
2025-06-07 11:23:13 -0400

    update2 june/7

--- src/main.py
+++ src/main.py
@@ -1,20 +1,28 @@
 from _12_copy_paste_function import copy_content, generate_page, generate_pages_recursive
 import sys
+import shutil
+import os
 origin = "./static"
 destination = "./docs"
 content = "./content"
 template = "./template.html"
+default_basepath = "/"
 
 # Right now our site always assumes that / is the root path of the site (e.g. http://localhost:8888/). Make if configurable by:
 
 # In main.py use the sys.argv to grab the first CLI argument to the program. Save it as the basepath. If one isn't provided, default to '/'. 
-basepath_list = sys.argv
-if len(basepath_list) > 1:
-    basepath = sys.argv[1]
-else:
-    basepath = "/"
-print(basepath)
+def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
 
-copy_content(origin, destination)
-generate_pages_recursive(content, template, destination, basepath)
+        print(f"this is the basepath: {basepath}")
 
+        if os.path.exists(destination):
+            shutil.rmtree(destination)
+
+    copy_content(origin, destination)
+    generate_pages_recursive(content, template, destination, basepath)
+
+
+main()
